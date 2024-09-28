89625d569ff060e01d3eac33dd76c97dd7119f7f
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-09-28 10:58:18 +0600

    appbar complete

---
+++ lib/screen/filter_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class FilterScreen extends StatefulWidget {
+  const FilterScreen({super.key});
+
+  @override
+  State<FilterScreen> createState() => _FilterScreenState();
+}
+
+class _FilterScreenState extends State<FilterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Filer the items from api"),
+      ),
+    );
+  }
+}
